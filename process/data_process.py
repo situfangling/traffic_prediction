7d18c082bf21fb232e9dd0b36957eabb9ed6da5a
situfangling <situfangling@163.com>
2017-04-22 19:21:18 +0800

    html added

---
+++ process/data_process.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+import csv
+
+FILE_NAME = "13301104001.csv"
+
+def solveSingleFile():
+    with open(FILE_NAME, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+
+    return
+
+
+def process():
+    return
+
+
+
+if __name__ =="__main__":
+    solveSingleFile()
+
+
