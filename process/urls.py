4c3c24eb10e4b2528d6510fe7a7d6fc6ecc7906f
jhljx <1261542265@qq.com>
2017-07-03 09:47:45 +0800

    modify some files

--- process/urls.py
+++ process/urls.py
@@ -3,7 +3,8 @@ from django.contrib.auth import views as auth_views
 from process.views import *
 
 urlpatterns = patterns('process.views',
- url(r'^index$','index',name='index'),
- url(r'^data_preprocess$','data_preprocess',name='data_preprocess'),
- url(r'^query_status$','query_status',name='query_status'),
+    url(r'^index$','index',name='index'),
+    url(r'^data_preprocess$','data_preprocess',name='data_preprocess'),
+    url(r'^query_status$','query_status',name='query_status'),
+    url(r'^query_area$','query_area', name='query_area'),
 )
--- process/views.py
+++ process/views.py
@@ -9,12 +9,14 @@ from process.helpers import pinyin_hash,region_hash2,week_hash,ajax_required,suc
 from process.data_process import process
 from traffic_prediction.settings import  BASE_DIR
 import datetime
-import os,pytz,random
+import os,pytz,random,json
 from process.tasks import process_data
 from process.models import CarRecord
 
+
 duration = 10
 RESULT = "result.txt"
+MAXINT = 999999999999
 
 tz_utc_8 = datetime.timezone(datetime.timedelta(hours=8)) # 创建时区UTC+8:00,防止构造datetime类时出现warning(强迫症添加)
 
@@ -41,7 +43,7 @@ def data_preprocess(request):
                 local_time = t_from + datetime.timedelta(hours=8)
                 #triple = [local_time.strftime("%Y-%m-%d %H:%M:%S"), count_num, i]
                 #print(triple)
-                out_line_str = local_time.strftime("%Y-%m-%d %H:%M:%S") + "," + str(record_num) + "," + str(i) + '\n'
+                out_line_str = local_time.strftime("%Y-%m-%d %H:%M:%S") + "," + str(record_num) + "," + str(region_id) + '\n'
                 file_object.writelines(out_line_str)
     print("finish")
     file_object.close()  ##忘了最后close这个文件，要细心
@@ -67,3 +69,15 @@ def query_status(request):
                 region_car_num = color * 600
                 region_color_dict["des"+str(region_id)] = region + u"<br/> 实时流量: " + str(region_car_num)
     return  success_response(**region_color_dict)
+
+@ajax_required
+def query_area(request):
+    point_list_tmp = request.POST.get('point_list','')
+    point_list = json.loads(point_list_tmp)
+    single_points_list = []
+    minX, maxX, minY, maxY = MAXINT, 0, MAXINT, 0
+    for point in point_list:
+        minX, maxX, minY, maxY = min(minX, point['lng']), max(maxX, point['lng']), min(minY, point['lat']), max(maxY,point['lat'])
+        single_points_list.append([point['lng'], point['lat']])
+    print("yes")
+
